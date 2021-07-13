c654f6360018197ef5edf2d7b6fb796db07747f5
Sai Vignesh <dragoonvignesh1998@gmail.com>
2021-07-13 23:26:44 +0530

    Made checks for scores and error messages

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 
 const secretNumber = Math.ceil(Math.random() * 20);
 console.log(secretNumber);
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number entered!';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '🔼Too high!';
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '🔽Too low!';
+
+  if (score > 0) {
+    if (!guess) {
+      document.querySelector('.message').textContent = '⛔ No number entered!';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = '🎉 Correct number!';
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = '🔼 Too high!';
+      score--;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = '🔽 Too low!';
+      score--;
+    }
+  } else {
+    document.querySelector('.message').textContent = '💀 You lost!';
   }
+  document.querySelector('.score').textContent = score;
 });
