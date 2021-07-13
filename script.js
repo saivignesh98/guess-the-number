564cb03d1e1b8d8e504d24642cdc03f2361afa74
Sai Vignesh <dragoonvignesh1998@gmail.com>
2021-07-13 17:13:46 +0530

    Added comparison code

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const secretNumber = Math.ceil(Math.random() * 20);
+console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  if (!guess)
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number entered!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '🔼Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '🔽Too low!';
+  }
 });
